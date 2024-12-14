e82bee9e20b2d238ee60c467db3560cf14cfa598
Aleh_Ladyka <alezhko.mail@gmail.com>
2024-12-14 12:31:39 +0300

    Update ads

--- TrainingDay.Maui/Extensions/ConstantKeys.cs
+++ TrainingDay.Maui/Extensions/ConstantKeys.cs
@@ -1,20 +1,20 @@
 ﻿namespace TrainingDay.Maui.Extensions;
 public class ConstantKeys
 {
-    public const string WorkoutsAndroidAds = "ca-app-pub-8728883017081055/2677919170";
-    public const string WorkoutsiOSAds = "ca-app-pub-8728883017081055/4777741279";
+    public const string WorkoutsAndroidAds = "ca-app-pub-5313002236683041/3406765721";
+    public const string WorkoutsiOSAds = "ca-app-pub-5313002236683041/6962867352";
 
-    public const string ImplementAndroidAds = "ca-app-pub-8728883017081055/4843502807";
-    public const string ImplementiOSAds = "ca-app-pub-8728883017081055/9745473409";
+    public const string ImplementAndroidAds = "ca-app-pub-5313002236683041/8012225141";
+    public const string ImplementiOSAds = "ca-app-pub-5313002236683041/1240499908";
 
-    public const string ExercisesAndroidAds = "ca-app-pub-8728883017081055/3839299823";
-    public const string ExercisesiOSAds = "ca-app-pub-8728883017081055/7119310061";
+    public const string ExercisesAndroidAds = "ca-app-pub-5313002236683041/7154439044";
+    public const string ExercisesiOSAds = "ca-app-pub-5313002236683041/7996747424";
 
-    public const string WorkoutAndroidAds = "ca-app-pub-8728883017081055/3839299823";
-    public const string WorkoutiOSAds = "ca-app-pub-8728883017081055/7119310061";
+    public const string WorkoutAndroidAds = "ca-app-pub-5313002236683041/6699143472";
+    public const string WorkoutiOSAds = "ca-app-pub-5313002236683041/5047017006";
 
-    public const string FinishWorkoutAndroidAds = "ca-app-pub-8728883017081055/7837401616";
-    public const string FinishWorkoutiOSAds = "ca-app-pub-8728883017081055/1550276858";
+    public const string FinishWorkoutAndroidAds = "ca-app-pub-5313002236683041/7746198508";
+    public const string FinishWorkoutiOSAds = "ca-app-pub-5313002236683041/5370584086";
 
     public const string NotFinishedTrainingName = "NotFinished.trday";
 
